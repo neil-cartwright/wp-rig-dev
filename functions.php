4dd7235089ef5b322d8416a73a55e0aa172df711
neil-cartwright <neil@sitepages.co.uk>
2020-03-19 08:02:02 +0000

    testing gulp

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * WP Rig functions and definitions
  *
@@ -9,11 +10,11 @@
  * @package wp_rig
  */
 
-define( 'WP_RIG_MINIMUM_WP_VERSION', '4.5' );
-define( 'WP_RIG_MINIMUM_PHP_VERSION', '7.0' );
+define('WP_RIG_MINIMUM_WP_VERSION', '4.5');
+define('WP_RIG_MINIMUM_PHP_VERSION', '7.0');
 
 // Bail if requirements are not met.
-if ( version_compare( $GLOBALS['wp_version'], WP_RIG_MINIMUM_WP_VERSION, '<' ) || version_compare( phpversion(), WP_RIG_MINIMUM_PHP_VERSION, '<' ) ) {
+if (version_compare($GLOBALS['wp_version'], WP_RIG_MINIMUM_WP_VERSION, '<') || version_compare(phpversion(), WP_RIG_MINIMUM_PHP_VERSION, '<')) {
 	require get_template_directory() . '/inc/back-compat.php';
 	return;
 }
@@ -22,7 +23,7 @@ if ( version_compare( $GLOBALS['wp_version'], WP_RIG_MINIMUM_WP_VERSION, '<' ) |
 require get_template_directory() . '/inc/wordpress-shims.php';
 
 // Setup autoloader (via Composer or custom).
-if ( file_exists( get_template_directory() . '/vendor/autoload.php' ) ) {
+if (file_exists(get_template_directory() . '/vendor/autoload.php')) {
 	require get_template_directory() . '/vendor/autoload.php';
 } else {
 	/**
@@ -33,22 +34,23 @@ if ( file_exists( get_template_directory() . '/vendor/autoload.php' ) ) {
 	 * @param string $class_name Class name to load.
 	 * @return bool True if the class was loaded, false otherwise.
 	 */
-	function _wp_rig_autoload( $class_name ) {
+	function _wp_rig_autoload($class_name)
+	{
 		$namespace = 'WP_Rig\WP_Rig';
 
-		if ( strpos( $class_name, $namespace . '\\' ) !== 0 ) {
+		if (strpos($class_name, $namespace . '\\') !== 0) {
 			return false;
 		}
 
-		$parts = explode( '\\', substr( $class_name, strlen( $namespace . '\\' ) ) );
+		$parts = explode('\\', substr($class_name, strlen($namespace . '\\')));
 
 		$path = get_template_directory() . '/inc';
-		foreach ( $parts as $part ) {
+		foreach ($parts as $part) {
 			$path .= '/' . $part;
 		}
 		$path .= '.php';
 
-		if ( ! file_exists( $path ) ) {
+		if (!file_exists($path)) {
 			return false;
 		}
 
@@ -56,11 +58,11 @@ if ( file_exists( get_template_directory() . '/vendor/autoload.php' ) ) {
 
 		return true;
 	}
-	spl_autoload_register( '_wp_rig_autoload' );
+	spl_autoload_register('_wp_rig_autoload');
 }
 
 // Load the `wp_rig()` entry point function.
 require get_template_directory() . '/inc/functions.php';
 
 // Initialize the theme.
-call_user_func( 'WP_Rig\WP_Rig\wp_rig' );
+call_user_func('WP_Rig\WP_Rig\wp_rig');
